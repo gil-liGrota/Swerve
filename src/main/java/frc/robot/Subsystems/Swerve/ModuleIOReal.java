322879da04e60749099c75ef55748c4e19c1ad85
gil-liGrota <gilgrota@gmail.com>
2025-06-04 20:41:18 +0300

    drive constants and working on module real

--- src/main/java/frc/robot/Subsystems/Swerve/ModuleIOReal.java
+++ src/main/java/frc/robot/Subsystems/Swerve/ModuleIOReal.java
@@ -1,9 +1,13 @@
 package frc.robot.Subsystems.Swerve;
 
+import java.util.Queue;
+
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkClosedLoopController;
 
+import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class ModuleIOReal extends ModuleIO{
@@ -17,7 +21,24 @@ public class ModuleIOReal extends ModuleIO{
     private final AbsoluteEncoder turnEncoder;
 
     // Close loop controllers
+    private final SparkClosedLoopController driveController;
+    private final SparkClosedLoopController turnController;
 
-    
+    // Queue inputs from odometry thread
+    private final Queue<Double> timestampQueue;
+    private final Queue<Double> drivePositionQueue;
+    private final Queue<Double> turnPositionQueue;
+
+    // Connection debouncers
+    private final Debouncer driveConnectedDebounce = new Debouncer(0.5);
+    private final Debouncer turnConnectedDebounce = new Debouncer(0.5);
+
+
+    public ModuleIOReal(int module){
+        zeroRotation = switch(module){
+            case 0 -> front
+        }
+    }
+
     
 }
