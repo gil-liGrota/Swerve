4f00eae39281b64c6590eb50c1c03e172f929d4e
gil-liGrota <gilgrota@gmail.com>
2025-06-04 20:08:42 +0300

    finish IO

---
+++ src/main/java/frc/robot/Subsystems/Swerve/ModuleIOReal.java
@@ -0,0 +1,23 @@
+package frc.robot.Subsystems.Swerve;
+
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public class ModuleIOReal extends ModuleIO{
+    
+    private final Rotation2d zeroRotation;
+
+    // Hardware objects
+    private final SparkBase driveSpark;
+    private final SparkBase turnSpark;
+    private final RelativeEncoder driveEncoder;
+    private final AbsoluteEncoder turnEncoder;
+
+    // Close loop controllers
+
+    
+    
+}
