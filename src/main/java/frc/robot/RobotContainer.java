07e7d6195fea09a4b61456a4efbaf7edb0cd908a
YehonatanEldar <yehonatan.eldar2@gmail.com>
2025-07-27 17:19:49 +0300

    trying tune the max axsekiration and velocity

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,10 +1,12 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Commands.SwerveCommands;
+import frc.robot.Commands.SwerveCommands.DriveToPosition;
 import frc.robot.Subsystems.Swerve.GyroIOPigeon;
 import frc.robot.Subsystems.Swerve.ModuleIOReal;
 import frc.robot.Subsystems.Swerve.Swerve;
@@ -12,6 +14,7 @@ import frc.robot.Subsystems.Swerve.Swerve;
 public class RobotContainer {
     private Swerve swerve = new Swerve(new GyroIOPigeon(), new ModuleIOReal(0), new ModuleIOReal(1),
             new ModuleIOReal(2), new ModuleIOReal(3));
+    private DriveToPosition driveToPosition = new DriveToPosition(swerve, new Pose2d(5, 0, null));
 
     private XboxController driverController = new XboxController(0);
 
@@ -19,12 +22,14 @@ public class RobotContainer {
         swerve.setDefaultCommand(
                 SwerveCommands.joystickDrive(swerve, driverController::getLeftY, driverController::getLeftX,
                         () -> driverController.getLeftTriggerAxis() - driverController.getRightTriggerAxis()));
+
         configureBindings();
     }
 
     private void configureBindings() {
 
         new Trigger(driverController::getYButton).onTrue(swerve.resetGyroCommand());
+        new Trigger(driverController::getAButton).onTrue(driveToPosition);
     }
 
     public Command getAutonomousCommand() {
