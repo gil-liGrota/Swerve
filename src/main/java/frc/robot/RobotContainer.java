edf12957de907ea76fd331607819bb9751f13a12
gil-liGrota <gilgrota@gmail.com>
2025-08-22 12:34:53 +0300

    Path planner working on air

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,8 +1,19 @@
 package frc.robot;
 
+import java.util.logging.Logger;
+
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.path.PathPlannerPath;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -13,6 +24,7 @@ import frc.robot.Subsystems.Swerve.ModuleIOReal;
 import frc.robot.Subsystems.Swerve.Swerve;
 
 public class RobotContainer {
+    private final LoggedDashboardChooser<Command> autoChooser;
     private Swerve swerve = new Swerve(new GyroIOPigeon(), new ModuleIOReal(0), new ModuleIOReal(1),
             new ModuleIOReal(2), new ModuleIOReal(3));
     private DriveToPosition driveToPosition = new DriveToPosition(swerve, new Pose2d(5, 0, null));
@@ -24,6 +36,9 @@ public class RobotContainer {
                 SwerveCommands.joystickDrive(swerve, driverController::getLeftY, driverController::getLeftX,
                         () -> driverController.getLeftTriggerAxis() - driverController.getRightTriggerAxis()));
         configureBindings();
+        autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
+
+        autoChooser.addDefaultOption("none", null);
     }
 
     private void configureBindings() {
@@ -35,6 +50,22 @@ public class RobotContainer {
     }
 
     public Command getAutonomousCommand() {
-        return Commands.print("No autonomous command configured");
+        // return Commands.print("No autonomous command configured");
+        // return new PathPlannerAuto("Example auto");
+        return autoChooser.get();
+    }
+
+    public Command runPath() {
+        try {
+            // Load the path you want to follow using its name in the GUI
+            PathPlannerPath path = PathPlannerPath.fromPathFile("Example Path");
+
+            // Create a path following command using AutoBuilder. This will also trigger
+            // event markers.
+            return AutoBuilder.followPath(path);
+        } catch (Exception e) {
+            DriverStation.reportError("Big oops: " + e.getMessage(), e.getStackTrace());
+            return Commands.none();
+        }
     }
 }
