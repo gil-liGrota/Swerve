acfcb895688099524abb63edd266f5f41146fbd6
YehonatanEldar <yehonatan.eldar2@gmail.com>
2025-08-26 19:39:58 +0300

    robot to camera finished

--- src/main/java/frc/robot/Subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/Subsystems/vision/VisionConstants.java
@@ -23,7 +23,7 @@ public class VisionConstants {
     // Robot to camera transforms
     // (Not used by Limelight, configure in web UI instead)
     public static Transform3d robotToCamera0 = new Transform3d(0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, 0.0));
-    public static Transform3d robotToCamera1 = new Transform3d(-0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, Math.PI));
+    public static Transform3d robotToCamera1 = new Transform3d(-0.1725, -0.0553, 0, new Rotation3d(0, 3.3329, Math.PI));
 
     // Basic filtering thresholds
     public static double maxAmbiguity = 0.3;
